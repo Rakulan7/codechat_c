53b6ea81ec788048078ab98fe52beb26cac7f957
Rakulan <s.rakulan04@gmail.com>
2024-02-05 10:30:46 +0100

    update

--- main.c
+++ main.c
@@ -1,29 +1,11 @@
-#include <SDL/SDL.h>
-
-int main() {
-
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_Surface * ecran = SDL_SetVideoMode(2000, 1000, 32, SDL_HWSURFACE);
-    SDL_Event event;
-
-    SDL_FillRect(ecran, NULL, SDL_MapRGB(ecran->format, 255, 239, 213));
-    SDL_Flip(ecran);
-
-
-
-
-
-
-    int active = 1;
-    while(active) {
-        SDL_WaitEvent(&event);
-        switch(event.type) {
-            case SDL_QUIT : active = 0; break;
-        }
-    }
-
-    SDL_FreeSurface(ecran);
-    SDL_Quit();
-    exit(EXIT_SUCCESS);
-
+#include <gtk/gtk.h>
+int main( int argc, char *argv[] )
+{
+ GtkWidget *window;
+ gtk_init (&argc, &argv);
+ window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+ gtk_widget_show (window);
+ gtk_main ();
+ return(0);
 }
+
